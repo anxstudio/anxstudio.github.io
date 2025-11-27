9e92b797dbb0bde5e9e71a3235214b13ce0c3825
AlexArgese <101403816+AlexArgese@users.noreply.github.com>
2025-11-27 16:02:12 +0100

    Fix vite base for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/website/', // <-- NOME DEL REPO QUI
   plugins: [react()],
   build: {
-    outDir: 'docs', // così la build finisce in /docs
+    outDir: 'docs',
   },
 })
